6bc4b5763e460e227d20800e93a05e75c107f7ca
Natanael Arndt <arndtn@gmail.com>
2018-05-17 15:26:39 +0200

    Add fix for gt and lt filters, FROM and non union default graph

--- adhs/__init__.py
+++
@@ -1,7 +0,0 @@
-import rdflib.plugins.sparql
-
-# To get the best behavior, but still we have https://github.com/RDFLib/rdflib/issues/810
-rdflib.plugins.sparql.SPARQL_DEFAULT_GRAPH_UNION = False
-
-# To disable web access: https://github.com/RDFLib/rdflib/issues/810
-rdflib.plugins.sparql.SPARQL_LOAD_GRAPHS = False
